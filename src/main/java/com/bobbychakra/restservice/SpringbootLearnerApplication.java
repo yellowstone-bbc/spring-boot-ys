ac457cd2ba400088236886073f54cee9e84838a7
yellow-stone-bbc <bobby-chakra@outlook.com>
2020-11-10 16:19:53 +0530

    Inital Commit

---
+++ src/main/java/com/bobbychakra/restservice/SpringbootLearnerApplication.java
@@ -0,0 +1,13 @@
+package com.bobbychakra.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bobbychakra/restservice/SpringbootLearnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobbychakra.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLearnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
